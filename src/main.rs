3373bacc4ad110cc737ada3a3342b311eadd4d2e
Harry <hccoo005@gmail.com>
2026-05-22 17:11:33 +0100

    - Added layout for list and todo info -Added keybinds for quitting the app after opening it

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use color_eyre::Result;
 use ratatui::DefaultTerminal;
 use ratatui::widgets::{Widget, StatefulWidget, Block, Borders, 
     Paragraph, List, ListItem, ListState};
-use ratatui::layout::{Layout, Rect, Constraint};
+use ratatui::layout::{Layout, Rect, Constraint, Direction};
 use ratatui::text::Line;
 use ratatui::buffer::Buffer;
-use crossterm::event::{self, Event, KeyEvent, KeyEventKind};
+use crossterm::event::{self, Event, KeyEvent, KeyEventKind, KeyCode};
 use std::io;
 #[derive(Debug)]
 struct App {
@@ -98,15 +98,49 @@ impl Default for App {
     }
 }
 
-impl Widget for &App {
-    fn render(self, area: Rect, buf: &mut Buffer) {}
+impl Widget for &mut App {
+    fn render(self, area: Rect, buf: &mut Buffer) {
+        let main_layout = Layout::horizontal([
+            Constraint::Length(40),
+            Constraint::Fill(1)
+        ]);
+        let [list_area, tab_area] = area.layout(&main_layout);
+        App::render_list(list_area, buf);
+        App::render_tab(tab_area, buf);
+    }
 }
 
 impl App {
+    fn render_list(area: Rect, buf: &mut Buffer) {
+        let block = Block::bordered();
+        Paragraph::new("there are a lot of peope").block(block).render(area, buf);
+    }
+
+
+    fn render_tab(area: Rect, buf: &mut Buffer) {
+        let tab_layout = Layout::vertical([
+            Constraint::Length(3),
+            Constraint::Fill(1)
+        ]);
+        let [tab_title_layout, tab_info_layout] = area.layout(&tab_layout);
+        App::render_tab_title(tab_title_layout, buf);
+        App::render_tab_info(tab_info_layout, buf);
+    }
+    fn render_tab_title(area: Rect, buf: &mut Buffer) {
+        let block = Block::bordered();
+        Paragraph::new("Title here").block(block).render(area, buf);
+    }
+    fn render_tab_info(area: Rect, buf: &mut Buffer) {
+        let block = Block::bordered();
+        Paragraph::new("A lot of text will go here just to describe what is happening").block(block).render(area, buf);
+
+    }
+
     fn run(mut self, terminal: &mut DefaultTerminal) -> Result<()> {
+        self.running = true;
         while self.running {
             terminal.draw(|frame| frame.render_widget(&mut self, frame.area()))?;
-            self.handle_events();
+            self.handle_events()?;
         }
         Ok(())
     }
@@ -123,13 +157,17 @@ impl App {
     
     fn handle_key_events(&mut self, key_event: KeyEvent) {
         match key_event.code {
-            //Quit
+            KeyCode::Char('q') => self.quit(),
             //Move through list
             //Adding items
             //Removing items
             _ => {}
         }
     }
-
-
+    
+    fn quit(&mut self) {
+        self.running = false;
+    }
 }
+
+
